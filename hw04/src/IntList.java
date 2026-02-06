48b24aa8e289b409e5c4bfda5fdb6f9f8a07ad1f
IronMan <myouter@outlook.com>
2026-02-06 21:20:46 +0800

    Finished hw04

--- hw04/src/IntList.java
+++ hw04/src/IntList.java
@@ -39,11 +39,14 @@ public class IntList {
      * each element incremented by x. L is not allowed
      * to change. Must use recursion.
      *
-     * This method is non-destructive, i.e. it must not modify the original list.
+     * This method is non-destructive非破坏性的, i.e. it must not modify the original
+     * list.
      */
     public static IntList incrRecursiveNondestructive(IntList L, int x) {
-        // TODO: Fill in this code
-        return null;
+        if (L == null) {
+            return null;
+        }
+        return new IntList(L.first + x, incrRecursiveNondestructive(L.rest, x));
     }
 
     /**
@@ -52,18 +55,34 @@ public class IntList {
      * You are not allowed to use "new" in this method.
      */
     public static IntList incrRecursiveDestructive(IntList L, int x) {
-        // TODO: Fill in this code
-        return null;
+        if (L == null) {
+            return null;
+        }
+
+        L.first += x;
+        incrRecursiveDestructive(L.rest, x);
+        return L;
     }
 
     /**
      * Returns an IntList identical to L, but with
      * each element incremented by x. Not allowed
      * to use recursion. May not modify the original list.
+     * 返回一个与L相同的IntList, 但每个元素的值都递增x.
+     * 不允许使用递归, 不得修改原始列表.
+     * 
+     * 
      */
     public static IntList incrIterativeNondestructive(IntList L, int x) {
-        // TODO: Fill in this code
-        return null;
+        IntList n = new IntList(L.first + x, null);
+        IntList m = n;
+        IntList p = L.rest;
+        while (p != null) {
+            m.rest = new IntList(p.first + x, null);
+            m = m.rest;
+            p = p.rest;
+        }
+        return n;
     }
 
     /**
@@ -73,8 +92,12 @@ public class IntList {
      * You are not allowed to use "new" in this method.
      */
     public static IntList incrIterativeDestructive(IntList L, int x) {
-        // TODO: Fill in this code
-        return null;
+        IntList p = L;
+        while (p != null) {
+            p.first += x;
+            p = p.rest;
+        }
+        return L;
     }
 
     /**
@@ -82,8 +105,16 @@ public class IntList {
      * elements of L2.
      */
     public static IntList concatenate(IntList L1, IntList L2) {
-        // TODO: Fill in this code
-        return null;
+        if (L2 == null)
+            return L1;
+        if (L1 == null)
+            return L2;
+        IntList p = L1;
+        while (p.rest != null && p.rest.rest != null) {
+            p = p.rest;
+        }
+        p.rest.rest = L2;
+        return L1;
     }
 
     /*
@@ -97,14 +128,28 @@ public class IntList {
      */
     public int sum() {
         // Optional: Fill in this code
-        return 0;
+        // if (rest == null)
+        // return first;
+        // return first + rest.sum();
+
+        int total = 0;
+        IntList p = this;
+        while (p != null) {
+            total += p.first;
+            p = p.rest;
+        }
+        return total;
     }
 
     /**
      * Destructively adds x to the end of the list.
      */
     public void addLast(int x) {
-        // Optional: Fill in this code
+        IntList p = this;
+        while (p != null && p.rest.rest != null) {
+            p = p.rest;
+        }
+        p.rest = new IntList(x, null);
     }
 
     /**
@@ -114,6 +159,13 @@ public class IntList {
      * be destructive.
      */
     public void addFirst(int x) {
-        // Optional: Fill in this code
+        // 关键技巧：创建一个新节点，其值为 x，rest 指向当前链表
+        IntList newFirst = new IntList(this.first, this.rest);
+
+        // 然后将当前节点的值改为 x
+        this.first = x;
+
+        // 最后将当前节点的 rest 指向刚刚创建的新节点
+        this.rest = newFirst;
     }
 }
